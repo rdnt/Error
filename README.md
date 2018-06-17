e6e888a3cd70ad970f63c36d31f956ec81e15050
ShtHappens796 <shithappens796@gmail.com>
2018-06-17 04:22:17 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Error
+An error page that displays when a user visits a subdomain that is not associated with a VirtualHost
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="robots" content="noindex">
+<title>SHT - Error</title>
+<link href="/css/style.css" type="text/css" rel="stylesheet" media="screen"/>
+<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+<link rel="manifest" href="/images/site.webmanifest">
+<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#0a0b0c">
+<link rel="shortcut icon" href="/images/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="SHT Error">
+<meta name="application-name" content="SHT Error">
+<meta name="msapplication-TileColor" content="#0a0b0c">
+<meta name="msapplication-config" content="/images/browserconfig.xml">
+<meta name="theme-color" content="#0a0b0c">
+</head>
+<body>
+<h6>OOPS</h6>
+<span>You're not supposed to be here.</span>
+<a href="https://www.sht.gr">SHT.GR</a>
+</body>
+</html>
