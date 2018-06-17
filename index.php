6cc800d78a23f3ec06f2beafbc82e107c5b1f063
ShtHappens796 <shithappens796@gmail.com>
2018-06-17 04:38:57 +0300

    Add commit version for cache refresh

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+function getLatestCommit() {
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    $master = "$root/.git/refs/heads/master";
+    if (file_exists($master)) {
+        return substr(file_get_contents($master), 0, 7);
+    }
+    else {
+        return "unknown";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,15 +19,15 @@
 <meta name="apple-mobile-web-app-title" content="SHT Error">
 <meta name="application-name" content="SHT Error">
 <meta name="msapplication-TileColor" content="#0a0b0c">
-<meta name="msapplication-config" content="/images/browserconfig.xml">
+<meta name="msapplication-config" content="/images/browserconfig.xml?v=<?=getLatestCommit()?>">
 <meta name="theme-color" content="#0a0b0c">
-<link href="/css/style.min.css" type="text/css" rel="stylesheet" media="screen"/>
-<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
-<link rel="manifest" href="/images/site.webmanifest">
-<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#0a0b0c">
-<link rel="shortcut icon" href="/images/favicon.ico">
+<link href="/css/style.min.css?v=<?=getLatestCommit()?>" type="text/css" rel="stylesheet" media="screen"/>
+<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png?v=<?=getLatestCommit()?>">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png?v=<?=getLatestCommit()?>">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png?v=<?=getLatestCommit()?>">
+<link rel="manifest" href="/images/site.webmanifest?v=<?=getLatestCommit()?>">
+<link rel="mask-icon" href="/images/safari-pinned-tab.svg?v=<?=getLatestCommit()?>" color="#0a0b0c">
+<link rel="shortcut icon" href="/images/favicon.ico?v=<?=getLatestCommit()?>">
 </head>
 <body>
 	<h1>OOPS</h1>
