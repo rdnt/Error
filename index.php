0e683f19e5d8b37e36b058c8bef920bdd11c1c55
ShtHappens796 <shithappens796@gmail.com>
2018-06-17 04:30:25 +0300

    Add charset meta

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="robots" content="noindex">
 <title>SHT - Error</title>
+<meta name="robots" content="noindex">
+<meta charset="UTF-8">
+<meta name="apple-mobile-web-app-title" content="SHT Error">
+<meta name="application-name" content="SHT Error">
+<meta name="msapplication-TileColor" content="#0a0b0c">
+<meta name="msapplication-config" content="/images/browserconfig.xml">
+<meta name="theme-color" content="#0a0b0c">
 <link href="/css/style.min.css" type="text/css" rel="stylesheet" media="screen"/>
 <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
@@ -10,15 +16,10 @@
 <link rel="manifest" href="/images/site.webmanifest">
 <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#0a0b0c">
 <link rel="shortcut icon" href="/images/favicon.ico">
-<meta name="apple-mobile-web-app-title" content="SHT Error">
-<meta name="application-name" content="SHT Error">
-<meta name="msapplication-TileColor" content="#0a0b0c">
-<meta name="msapplication-config" content="/images/browserconfig.xml">
-<meta name="theme-color" content="#0a0b0c">
 </head>
 <body>
-<h6>OOPS</h6>
-<span>You're not supposed to be here.</span>
-<a href="https://www.sht.gr">SHT.GR</a>
+	<h6>OOPS</h6>
+	<span>You're not supposed to be here.</span>
+	<a href="https://www.sht.gr">SHT.GR</a>
 </body>
 </html>
